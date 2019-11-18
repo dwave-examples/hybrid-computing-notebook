4247f766978fd89a089ef7be4042f03341e0b9f1
Joel <34041130+JoelPasvolsky@users.noreply.github.com>
2019-11-18 14:18:35 -0800

    Add new hybrid-computing JN in Leap  (#122)
    * Add empty hybrid JN
    
    * Add problem and out-of-box sampler
    
    * Add Workflow section and Kerberos workflow
    
    * Replace workflow with PT
    
    * Add custom component GreedyRandomProblemSampler and expand custom workflow
    
    * Flesh out some descriptions
    
    * Add steps to custom component section
    
    * Flesh out first section until customizing sampler
    
    * Cover --> cover
    
    * Update Reference Workflow PT
    
    * Flesh out Custom Workflows: simple, branches
    
    * Add Selecting Samples section
    
    * Add Iterating subsection
    
    * Move hybrid JN
    
    * Split JN into three notebooks
    
    * Add advanced workflows, add subplotting graphics
    
    * Add Decomposing section, update graphics code
    
    * Add content to 02 Runnables section and remove Qt5Agg
    
    * Add exercises to 01 and update Kerberos description
    
    * Add branch code including exercise
    
    * Add state traits and more code and exercises to 02 branch section
    
    * Add code plus exercise to 02 selection subsection
    
    * Change content of 02 Decomposition subsection
    
    * Add 02 energy-tracking exercise and lambda exercise
    
    * Fix some code at EOD
    
    * Modify exercise template in 02
    
    * Modify exercise template in 01
    
    * 02 workflows JN first draft (coded Sample Workflows section)
    
    * Add 01 first Runnable code cells
    
    * Add futures and stop method code
    
    * Add 03 Traits section
    
    * Add 03 States section and interlinks
    
    * Rename without Custom
    
    * Restructure 03 for Working With versus Writing components
    
    * Rename 01 and update contents for 1st draft
    
    * Fix some minor bugs in 01
    
    * Updated 03 Runnables content and propagated some naming changes
    
    * Tidy up 03 code
    
    * Update 03 to keep original state for modified runnables
    
    * Add 03 MIMO exercise
    
    * Add 03 MIMO exercise
    
    * Begin adding 03 custom sampler
    
    * Add 03 IncrementCountPrintEnergy exercise
    
    * Add 03 custom sampler TabuProblemSamplerFiltered
    
    * Add 03 dimod conversion
    
    * Begin preparing review draft
    
    * Prepare 01 for initial review
    
    * Prepare 02 for initial review (reached 1st cell of Runnables section
    
    * Prepare 02 for initial review (reached exercise of Iterating
    
    * Prepare 02 for initial review
    
    * Update landing page with hybrid computing
    
    * Prepare 03 for initial review (until Conversion section)
    
    * Updated 01, 02 for almost all Radomir's initial feedback
    
    * Updated 03 for Radomir's initial feedback
    
    * Update remaining initial feedback from Radomir
    
    * Update 03 Conversion to use #213 and #215
    
    * 01 code review version
    
    * 02 code review version (until Selecting Samples section)
    
    * 02 code review version
    
    * Add 03 2nd conversion example
    
    * 03 code review version
    
    * Implement review comments from @vgoliber
    
    * JN 01: implement review comments from @m3ller and @davage
    
    * JN 02: implement review comments from @m3ller and @davage
    
    * JN 01 & 02: implement review comments from @mdecandia
    
    * JN 03: implement review comments from @m3ller and  @mdecandia
    
    * JN 01 & 02 minor updates pre-deploy
    
    * JN 03 minor updates pre-deploy

---
+++ helpers/draw.py
@@ -0,0 +1,81 @@
+# Copyright 2019 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Helper plot functions
+"""
+
+import numpy as np
+import networkx as nx
+
+import matplotlib.pyplot as plt
+import matplotlib
+# `Qt5Agg` works well in ipython/console
+#matplotlib.use('Qt5Agg')
+from matplotlib.gridspec import GridSpec
+
+import dwave_networkx as dnx
+import dimod
+
+
+def plot(G, subgraphs=None, max_subs=3, subtitles=False, pos=None):
+    """Plot utility"""
+
+    if pos is None:
+        pos = nx.get_node_attributes(G, 'pos')
+    if not pos:
+        pos = nx.spring_layout(G)
+
+    if not subgraphs:
+        axes = 1
+        graph_edge_alpha = 0.2
+        graph_node_alpha = 0.9
+
+    else:
+        axes = min(max_subs, len(subgraphs))
+        graph_edge_alpha = 0.05
+        graph_node_alpha = 0.1
+
+        subgraph_edge_alpha = 0.4
+        subgraph_node_alpha = 0.9
+
+    fig = plt.figure(figsize=(15,5))
+    gs = GridSpec(1, axes)
+    
+    for i in range(axes):
+        ax = 'ax_' + str(i)
+        ax = plt.subplot(gs[0, i])
+        if subtitles:
+            ax.set_title("Subproblem "+str(i+1))
+           
+        # edges for full graph
+        nx.draw_networkx_edges(G, pos, alpha=graph_edge_alpha)
+
+        # edges for subgraph
+        if subgraphs:
+            nx.draw_networkx_edges(subgraphs[i], pos, alpha=subgraph_edge_alpha)
+
+        # nodes for full graph
+        nodelist = G.nodes.keys()
+        max_degree = max(dict(G.degree).values())
+
+        # top 70% of reds cmap
+        normalized_degrees = [0.3 + 0.7 * G.degree[n] / max_degree for n in nodelist]
+        node_color = plt.cm.Reds([0] + normalized_degrees)[1:]
+        nx.draw_networkx_nodes(G, pos, nodelist=nodelist, node_size=80, node_color=node_color, alpha=graph_node_alpha)
+
+        # nodes for subgraph
+        if subgraphs:
+            sub_nodelist = subgraphs[i].nodes.keys()
+            sub_node_color = [node_color[n] for n in sub_nodelist]
+            nx.draw_networkx_nodes(subgraphs[i], pos, nodelist=sub_nodelist, node_size=80, node_color=sub_node_color, alpha=subgraph_node_alpha)
